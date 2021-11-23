9eaee07293b0798a8083c1bb8e71d106f98858c9
Jarikkomarik <ufland.ufland@gmail.com>
2021-11-23 23:13:49 +0100

    Updated realization of Factory Method Pattern.

--- src/main/java/FactoryMethodPattern/CarFactory.java
+++ src/main/java/FactoryMethodPattern/CarFactory.java
@@ -2,4 +2,11 @@ package FactoryMethodPattern;
 
 abstract class CarFactory { //creator
     public abstract Car createCar(String make);
+
+    public Car produceCar (String make){
+        Car producedCar = createCar(make);
+        System.out.println("Crating " +make);
+        System.out.println(make + " is ready!");
+        return producedCar;
+    }
 }
--- src/main/java/FactoryMethodPattern/Client.java
+++ src/main/java/FactoryMethodPattern/Client.java
@@ -3,15 +3,28 @@ package FactoryMethodPattern;
 public class Client { // client side
     public static void main(String[] args) {
         CarDealer dealer = new CarDealer();
-        Car mercedes = dealer.createCar("mercedes");
-        Car audi = dealer.createCar("audi");
-        Car tesla = dealer.createCar("tesla");
+        Car mercedes = dealer.produceCar("mercedes");
+        System.out.println();
+        Car audi = dealer.produceCar("audi");
+        System.out.println();
+        Car tesla = dealer.produceCar("tesla");
+        System.out.println();
         mercedes.printSpeedLimit();
         audi.printSpeedLimit();
         tesla.printSpeedLimit();
+        System.out.println();
 
         /*
         Output
+        Crating mercedes
+        mercedes is ready!
+
+        Crating audi
+        audi is ready!
+
+        Crating tesla
+        tesla is ready!
+
         Mercedes speed limit is 240 km/h!
         Audi speed limit is 260 km/h!
         Tesla speed limit is 180 km/h!
@@ -19,10 +32,14 @@ public class Client { // client side
 
         //to create new car. Instantiate another Car and add Dealer option for desired car
 
-        Car bentley = dealer.createCar("bentley");
+        Car bentley = dealer.produceCar("bentley");
+        System.out.println();
         bentley.printSpeedLimit();
         /*
         Output
+        Crating bentley
+        bentley is ready!
+
         Bentley speed limit is 300 km/h!
         */
     }
